e7efd3df0cb97816422c15db0b25a7b3c37361b3
Cyber MacGeddon <cybermaggedon@gmail.com>
2022-04-19 22:21:13 +0100

    Asyncio on firebase client (broke everything)

--- accountsmachine/state/store.py
+++ accountsmachine/state/store.py
@@ -8,21 +8,22 @@ from firebase_admin import firestore
 logger = logging.getLogger("store")
 logger.setLevel(logging.DEBUG)
 
-class Collection:
+class DocCollection:
     def __init__(self, db, collection):
         self.db = db
         self.collection = db.collection(collection)
-    def __setitem__(self, key, item):
-        self.collection.document(key).set(item)
-    def __getitem__(self, key):
-        doc = self.collection.document(key).get()
+    async def set(self, key, item):
+        await self.collection.document(key).set(item)
+    async def get(self, key):
+        doc = await self.collection.document(key).get()
         if not doc.exists:
             raise KeyError()
         return doc.to_dict()
-    def __delitem__(self, key):
-        self.collection.document(key).delete()
+    async def delete(self, key):
+        await self.collection.document(key).delete()
     def all(self, key, value):
-        docs = [v for v in self.collection.where(key, '==', value).stream()]
+        qry = self.collection.where(key, '==', value)
+        docs = [v for v in qry.stream()]
         return {
             doc.id.split("@")[0]: doc.to_dict()
             for doc in docs
@@ -32,27 +33,27 @@ class DocStore:
     def __init__(self, config, firebase):
 
         logger.debug("Opening firestore...")
-        self.db = firestore.client()
+        self.db = firestore.AsyncClient()
         logger.debug("Opened")
 
     def collection(self, coll):
-        return Collection(self.db, coll)
+        return DocCollection(self.db, coll)
 
     async def get(self, coll, id):
         logger.debug("get %s %s" % (coll, id))
-        return self.collection(coll)[id]
+        return await self.collection(coll).get(id)
 
     async def put(self, coll, id, data):
         logger.debug("put %s %s" % (coll, id))
-        self.collection(coll)[id] = data
+        await self.collection(coll).set(id, data)
 
     async def delete(self, coll, id):
         logger.debug("delete %s %s" % (coll, id))
-        del self.collection(coll)[id]
+        await self.collection(coll).delete(id)
 
     async def get_all(self, coll, key, value):
         logger.debug("get_all %s" % coll)
-        data = self.collection(coll).all(key, value)
+        data = await self.collection(coll).all(key, value)
         return data
 
 class BlobStore:
